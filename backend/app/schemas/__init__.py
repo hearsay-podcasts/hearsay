bb0f41aad3849939681887a51bc0af858f6d7e11
Samuel Sarzaba <samuelsarzaba@gmail.com>
2026-01-07 21:04:46 -0500

    Deleted backend

--- backend/app/schemas/__init__.py
+++
@@ -1,4 +0,0 @@
-from app.schemas.token import Token, TokenPayload
-from app.schemas.user import UserCreate, UserResponse, UserUpdate
-
-__all__ = ["Token", "TokenPayload", "UserCreate", "UserResponse", "UserUpdate"]
