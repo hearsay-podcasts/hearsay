d540a82fb6f0a714394e57c05f32373b62fc0192
Samuel Sarzaba <samuelsarzaba@gmail.com>
2026-01-16 21:36:56 -0500

    Reset SvelteKit

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,16 +1,2 @@
-<script lang="ts">
-	import Hero from './Hero.svelte';
-	import CallToAction from './CallToAction.svelte';
-	import PopularPodcasts from './PopularPodcasts.svelte';
-</script>
-
-<svelte:head>
-	<title>Hearsay - Track Your Favorite Podcasts</title>
-	<meta name="description" content="Track and manage your favorite podcasts in one place" />
-</svelte:head>
-
-<main class="w-full">
-	<Hero />
-	<CallToAction />
-	<PopularPodcasts />
-</main>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
